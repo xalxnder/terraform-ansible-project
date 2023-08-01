0f8ac11dc46ceed0f88d0e1a304f72f2a178bc28
Xavier Alexander <xalexander51@gmail.com>
2023-08-01 06:07:26 -0400

    Update main.tf
    - Add random resource
    - Add lifecycle policy

--- main.tf
+++ main.tf
@@ -1,10 +1,28 @@
+resource "random_id" "random" {
+  byte_length = 2
+}
+
 resource "aws_vpc" "architech_vpc" {
   cidr_block           = var.vpc_cidr
   enable_dns_hostnames = true
   enable_dns_support   = true
 
   tags = {
-    Name = "architech_vpc"
+    Name = "architech_vpc-${random_id.random.dec}"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+}
+
+
+resource "aws_internet_gateway" "architech_internet_gateway" {
+  vpc_id = aws_vpc.architech_vpc.id
+
+  tags = {
+    Name = "architech_gateway-${random_id.random.dec}"
   }
 
 }
