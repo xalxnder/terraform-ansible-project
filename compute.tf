7eec7ca8165cb42a9fe95977ceabe7b3e36deed6
Xavier Alexander <xalexander51@gmail.com>
2023-08-18 05:52:47 -0400

    Update SSH Keys

--- compute.tf
+++ compute.tf
@@ -15,8 +15,8 @@ data "aws_ami" "server_ami" {
 
 }
 
-resource "aws_key_pair" "main_key" {
-  key_name   = "main_key"
+resource "aws_key_pair" "architech_key" {
+  key_name   = "architech_key"
   public_key = file(var.TERRAFORM_KEY)
 
 }
@@ -25,10 +25,9 @@ resource "aws_instance" "architech_main" {
   count                  = var.main_instance_count
   instance_type          = var.main_instance_type
   ami                    = data.aws_ami.server_ami.id
-  key_name               = aws_key_pair.main_key.id
+  key_name               = aws_key_pair.architech_key.id
   vpc_security_group_ids = [aws_security_group.architech_security.id]
   subnet_id              = aws_subnet.architech_public_subnet[count.index].id
-  user_data              = templatefile("./main-user-data.tpl", { new_hostname = "architech_main-${random_id.architech_node_id[count.index].dec}" })
   root_block_device {
     volume_size = var.main_vol_size
   }
@@ -38,7 +37,7 @@ resource "aws_instance" "architech_main" {
   }
 
   provisioner "local-exec" {
-    command = "printf '\n${self.public_ip}' >> aws_hosts"
+    command = "printf '\n${self.public_ip}' >> aws_hosts && aws ec2 wait instance-status-ok --instance-ids ${self.id} --region us-east-1"
   }
 
   provisioner "local-exec" {
@@ -47,5 +46,13 @@ resource "aws_instance" "architech_main" {
   }
 }
 
+
+resource "null_resource" "grafana_install" {
+  depends_on = [aws_instance.architech_main]
+  provisioner "local-exec" {
+    command = "ansible-playbook -i aws_hosts --key-file /Users/xalexander/.ssh/architech_key.pub playbooks/grafana.yml"
+
+  }
+}
 
 
